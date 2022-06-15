d6cc07a95716b0a6e6e5182e13c67f06cce9c6cf
Balavishnu V J <hello@balavishnuvj.com>
2022-06-15 20:41:35 +0530

    fix: build for test

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./dist/react-hooks-form-validator.cjs');
-} else {
+if (process.env.NODE_ENV === 'development') {
   module.exports = require('./src/index.js');
+} else {
+  module.exports = require('./dist/react-hooks-form-validator.cjs');
 }
